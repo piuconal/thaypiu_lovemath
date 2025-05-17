d6d625652ce595e63b2c8eb1201f9af355a169fb
Vũ Trọng Hiếu <85973353+piuconal@users.noreply.github.com>
2025-05-17 21:24:43 +0700

    up Toan12

--- include/bai_js.js
+++ include/bai_js.js
@@ -61,3 +61,7 @@ function closeModal() {
   const modal = document.getElementById("imageModal");
   modal.style.display = "none";
 }
+function toggleMenu() {
+  const navMenu = document.getElementById("navMenu");
+  navMenu.classList.toggle("show");
+}
